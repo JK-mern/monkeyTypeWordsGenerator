9d789da76440d7b14000b18faeaff26af5fa1dc4
jkmerndev <jkmerndev@gmail.com>
2024-11-05 13:09:02 +0530

    created words generator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("jayakrishnan")
+import { LoremIpsum } from "lorem-ipsum";
+
+export const wordsGenerator = () => {
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4,
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4,
+    },
+  });
+
+  const words = lorem.generateParagraphs(1)
+  console.log(words)
+};
+
+
+
+wordsGenerator()
