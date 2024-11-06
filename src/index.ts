5e2068401fc4467df7fb8eb331f248a879ccabad
jkmerndev <jkmerndev@gmail.com>
2024-11-06 23:15:40 +0530

    created a function to render words for 30 second typing test

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import { LoremIpsum } from "lorem-ipsum";
+import { words } from "./utils/words";
+function generateTextFor30Sec() {
+  
+  const allWords = [
+      ...words.nouns, 
+      ...words.verbs, 
+      ...words.adjectives, 
+      ...words.adverbs
+  ];
+  let text = [];
+  for (let i = 0; i < 100; i++) {
+      const randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+      text.push(randomWord);
+  }
+  return text.join(" ");
+}
 
-export const wordsGenerator = () => {
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 4,
-    },
-    wordsPerSentence: {
-      max: 16,
-      min: 4,
-    },
-  });
+console.log(generateTextFor30Sec())
 
-  const words = lorem.generateParagraphs(1)
-  console.log(words)
-};
-
-
-
-wordsGenerator()
---
+++ src/utils/words.ts
@@ -0,0 +1,26 @@
+export const words = {
+    nouns: [
+        "journey", "harmony", "wisdom", "innovation", "freedom", "destiny", 
+        "vision", "challenge", "concept", "experience", "ambition", "purpose", 
+        "strength", "community", "growth", "opportunity", "solution", "adventure", 
+        "future", "dream"
+    ],
+    verbs: [
+        "explore", "achieve", "create", "inspire", "discover", "connect", 
+        "transform", "enhance", "understand", "embrace", "pursue", "empower", 
+        "design", "develop", "nurture", "succeed", "support", "build", "reflect", 
+        "motivate"
+    ],
+    adjectives: [
+        "exciting", "innovative", "dynamic", "meaningful", "resilient", "brave", 
+        "curious", "inclusive", "passionate", "purposeful", "creative", "ambitious", 
+        "supportive", "bold", "collaborative", "compassionate", "thoughtful", 
+        "driven", "dedicated", "unique"
+    ],
+    adverbs: [
+        "thoughtfully", "passionately", "boldly", "creatively", "authentically", 
+        "enthusiastically", "constantly", "continuously", "genuinely", "eagerly", 
+        "wholeheartedly", "proudly", "resolutely", "deliberately", "honestly", 
+        "humbly", "purposefully", "effectively", "frequently", "easily"
+    ]
+};
